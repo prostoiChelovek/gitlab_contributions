7847a46806d75844151c36394a39b3796aadce84
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2020-05-21 07:50:02 +0000

    add code in files types: .py remove code in files types: .py

---
+++ content.py
@@ -0,0 +1,17 @@
+import sys
+import git
+
+sys.path.append("Contributions-Importer-For-Github/")
+
+from git_contributions_importer import *
+
+repo_path = sys.argv[1] if len(sys.argv) > 1 else "/tmp/first"
+
+repo = git.Repo(repo_path)
+mock_repo = git.Repo(".")
+
+importer = Importer([repo], mock_repo)
+importer.set_author(['i.prostoi.chelovek@yandex.ru'])
+
+importer.set_start_from_last(True)
+print("chpxb")
