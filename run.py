8d2b938d022e7870f29d44dfe76d873d94fe2a15
prostoChelovek <i.prostoi.chelovek@yandex.ru>
2021-01-06 17:49:42 +0500

    add importer's dir to python path

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import sys
 import git
 
+sys.path.append("Contributions-Importer-For-Github/")
+
 from git_contributions_importer import *
 
 repo_path = sys.argv[1] if len(sys.argv) > 1 else "/tmp/first"
