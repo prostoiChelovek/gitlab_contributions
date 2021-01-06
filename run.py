e6c7632d4f6e0b945dd5fb1326ff59f45fe1e42d
prostoChelovek <i.prostoi.chelovek@yandex.ru>
2021-01-06 18:13:52 +0500

    prevent importer from duplicating commits next time

--- run.py
+++ run.py
@@ -13,5 +13,5 @@ mock_repo = git.Repo(".")
 importer = Importer([repo], mock_repo)
 importer.set_author(['i.prostoi.chelovek@yandex.ru'])
 
+importer.set_start_from_last(True)
 importer.import_repository()
-# importer.set_start_from_last(True)
